8c8ed026006ab9d3155aa54b5d89d3c7aaa432c7
kiavashp <kiavashp@gmail.com>
2015-11-16 21:33:51 -0800

    normalize tabs

--- supersicht.js
+++ supersicht.js
@@ -1,77 +1,78 @@
 }),(function widget(exports, compile) {
 
-	var _this = this;
-	var DEBUG = true;
-	var DIRNAME = '_supersicht';
-	var WIDGETS_DIR;
-	var DEFAULT_WIDGET;
-	var fs;
-	var path;
-	var widget = {
-		command: 'echo',
-		refreshFrequency: false,
-		style: 'display:none',
-		render: function () {
-			var _this;
-			return _this.error || '';
-		},
-		afterRender: function (element){
-	        var parent = element.parentNode;
-	        parent.parentNode.removeChild(parent);
-	    }
-	};
+    var _this = this;
+    var DEBUG = true;
+    var DIRNAME = '_supersicht';
+    var WIDGETS_DIR;
+    var DEFAULT_WIDGET;
+    var fs;
+    var path;
+    var widget = {
+        command: 'echo',
+        refreshFrequency: false,
+        style: 'display:none',
+        render: function () {
+            var _this;
+            return _this.error || '';
+        },
+        afterRender: function (element){
+            var parent = element.parentNode;
+            parent.parentNode.removeChild(parent);
+        }
+    };
 
-	try {
+    try {
 
-		fs = require('fs');
-		path = require('path');
+        fs = require('fs');
+        path = require('path');
 
-		WIDGETS_DIR = path.resolve(path.dirname(filePath), '../');
-	    DEFAULT_WIDGET = readFile(DIRNAME +'/lib/default-widget');
+        WIDGETS_DIR = path.resolve(path.dirname(filePath), '../');
+        DEFAULT_WIDGET = readFile(DIRNAME +'/lib/default-widget');
 
+        sdfsdf
     } catch (e) {
 
-		if (DEBUG) {
-			widget.error = e.stack;
-		}
+        if (DEBUG) {
+            widget.error = e.stack;
+        }
 
     }
 
-	function _resolveWidgetPath(filepath) {
-		return path.resolve(WIDGETS_DIR, filepath);
-	}
+    function _resolveWidgetPath(filepath) {
+        return path.resolve(WIDGETS_DIR, filepath);
+    }
 
-	function defaultWidget(){
-		return compile(DEFAULT_WIDGET);
-	}
+    function defaultWidget(){
+        return compile(DEFAULT_WIDGET);
+    }
 
-	function runInNode (func) {
-		return typeof func === 'function' && func();
-	}
+    function runInNode (func) {
+        return typeof func === 'function' && func();
+    }
 
-	function readFile(filepath, options) {
-		var realpath = _resolveWidgetPath(filepath);
-		return fs.readFileSync(realpath, options);
-	}
+    function readFile(filepath, options) {
+        var realpath = _resolveWidgetPath(filepath);
+        return fs.readFileSync(realpath, options);
+    }
 
-	function requireFile(filepath) {
-		var fileresults = require(_resolveWidgetPath(filepath));
-		return fileresults;
-	}
+    function requireFile(filepath) {
+        var fileresults = require(_resolveWidgetPath(filepath));
+        return fileresults;
+    }
 
-	function loadFromConfig(filepath) {
-		var widget = readFile(filepath) || DEFAULT_WIDGET;
-		widget = compile(widget);
-		return widget;
-	}
+    function loadFromConfig(filepath) {
+        var widget = readFile(filepath) || DEFAULT_WIDGET;
+        widget = compile(widget);
+        return widget;
+    }
 
-	exports.defaultWidget = defaultWidget;
-	exports.runInNode = runInNode;
-	exports.readFile = readFile;
-	exports.require = requireFile;
-	exports.loadFromConfig = loadFromConfig;
+    exports.defaultWidget = defaultWidget;
+    exports.runInNode = runInNode;
+    exports.readFile = readFile;
+    exports.require = requireFile;
+    exports.loadFromConfig = loadFromConfig;
 
-	return widget;
+    return widget;
 
 }(supersicht=widget=this,function(_){try{return JSON.parse(_)}catch(e){try{return eval('('+_+')')}catch(e){}}}))
 //
