4d6e53975f8d425b57662971c5a71a0203218400
kiavashp <kiavashp@gmail.com>
2015-11-22 12:35:47 -0800

    add supersicht.log

--- supersicht.js
+++ supersicht.js
@@ -5,28 +5,39 @@
     var DIRNAME = '_supersicht';
     var WIDGETS_DIR;
     var DEFAULT_WIDGET;
+    var util;
     var fs;
     var path;
-    var widget = {
+    var supersicht = {
         command: 'echo',
         refreshFrequency: false,
         style: 'display:none',
-        render: function () {
+        render: function render() {
             var _this = this;
-            return _this.error || '';
+            return '<pre>' + (_this.logs || '') +'</pre>';
         },
-        afterRender: function (element){
+        afterRender: function afterRender(element){
             var _this = this;
-            if (_this.error) {
+            if (_this.logs) {
                 return;
             }
             var parent = element.parentNode;
             parent.parentNode.removeChild(parent);
+        },
+        log: function log() {
+            var _this = this;
+            if (!_this.logs) {
+                _this.logs = '';
+            }
+            _this.logs += [].slice.call(arguments).map(function (a) {
+                return typeof a === 'string' ? a : util.inspect(a);
+            }).join(' ') + '\n';
         }
     };
 
     try {
 
+        util = require('util');
         fs = require('fs');
         path = require('path');
 
@@ -36,7 +47,7 @@
     } catch (e) {
 
         if (DEBUG) {
-            widget.error = e.stack;
+            supersicht.log(e.stack);
         }
 
     }
@@ -75,7 +86,7 @@
     exports.require = requireFile;
     exports.loadFromConfig = loadFromConfig;
 
-    return widget;
+    return supersicht;
 
 }(supersicht=widget=this,function(_){try{return JSON.parse(_)}catch(e){try{return eval('('+_+')')}catch(e){}}}))
 //
